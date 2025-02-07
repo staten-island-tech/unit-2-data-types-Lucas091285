ea8da49464bfa1cfa8dca074be548008f2d6ff4f
Lucas091285 <lucasl107@nycstudents.net>
2025-02-07 09:25:45 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+"""  x = 3
+y = float(3)
+print(x,y) """
+
+ values = []
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6]) 
+
+ x = "This is a box"
+y = x.split()
+z = y[0]
+print(y)
+print(z)
