a06f20ad2ddc70e3c19d6d74ca87a46792e2595e
Lucas091285 <lucasl107@nycstudents.net>
2025-02-10 09:21:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,27 @@
 y = float(3)
 print(x,y) """
 
- values = []
-print(values)
+"""  values = []
+print(This is a thing)
 for i in values:
-    print(i)
+    print(i) """
 
-print(values[0])
-print(values[6]) 
+""" print(values[0])
+print(values[6])  """
 
- x = "This is a box"
-y = x.split()
+""" "This is a thing"
+x = "This is a thing"
+y= x.split( )
 z = y[0]
 print(y)
-print(z)
+print(z) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
