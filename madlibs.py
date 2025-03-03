ac6677f661d2e815c7cba178d7c5f8c35301eb65
Lucas091285 <lucasl107@nycstudents.net>
2025-03-03 08:47:36 -0500

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+verb1 = input("Enter a verb:")
+verb2 = input("Enter a verb:")
+noun = input("Enter a noun:")
+number = input("Enter a number:")
+celebrity = input("Enter a celebrity:")
+
+MadLibs = f"Hello, I am {verb1} for {noun} birthday. He is turning {number} today. Our special guest is {celebrity}. I am very {verb2}"
+print(MadLibs)
