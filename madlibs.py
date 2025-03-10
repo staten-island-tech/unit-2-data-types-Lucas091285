9fb3df51ca0a9d6fe471bcb075511b8503a78b76
Lucas091285 <lucasl107@nycstudents.net>
2025-03-10 08:56:12 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -5,3 +5,4 @@ number = input("Enter a number:")
 celebrity = input("Enter a celebrity:")
 
 MadLibs = f"Hello, I am {verb1} for {noun} birthday. He is turning {number} today. Our special guest is {celebrity}. I am very {verb2}"
+print(MadLibs)
